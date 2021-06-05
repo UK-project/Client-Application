a8a8ac626644c3479c90bc9a6196a0f62d080cfc
inshaf ahamed <amdinshaf@gmail.com>
2021-06-05 23:12:25 +0530

    Added data to Datagrid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+//todo: from separate file
+const theme = createMuiTheme({
+  typography: {
+    allVariants: {
+      color: "white",
+    },
+  },
+  palette: {
+    primary: {
+      main: "#2e3f47",
+    },
+    secondary: {
+      main: "#f94a4a",
+    },
+    background: {
+      default: "#233036",
+      paper: "#2e3f47",
+    },
+    text: {
+      primary: "#ffffff",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Dashboard />
+      </div>
+    </ThemeProvider>
   );
 }
 
